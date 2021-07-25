1fc40eba2df9d51481f26b2bee3b14dac6107307
shijun18 <46990068+shijun18@users.noreply.github.com>
2021-07-25 18:52:27 +0800

    update

--- ml_classifier/mlp_run.py
+++ ml_classifier/mlp_run.py
@@ -1,18 +1,20 @@
 import os
+import shutil
+import pickle
 from torch import nn
 import torch
 import pandas as pd
-
+import random
 import numpy as np
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.cuda.amp import autocast,GradScaler
-
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
-
 from sklearn.metrics import accuracy_score,f1_score
 from utils import dfs_remove_weight
+from feature_selection import select_feature_linesvc
+
 
 class MyDataset(Dataset):
     def __init__(self, X, Y=None):
@@ -34,7 +36,7 @@ class MyDataset(Dataset):
 
 
 class MLP_CLASSIFIER(nn.Module):
-    def __init__(self, input_size, output_size=245, depth=3, depth_list=[256,128,64], drop_prob=0.5):
+    def __init__(self, input_size, output_size=245, depth=3, depth_list=[256,128,64], drop_prob=0.5, use_norm=True):
         super(MLP_CLASSIFIER, self).__init__()
 
         assert len(depth_list) == depth
@@ -44,8 +46,12 @@ class MLP_CLASSIFIER(nn.Module):
                 self.linear_list.append(nn.Linear(input_size,depth_list[i]))
             else:
                 self.linear_list.append(nn.Linear(depth_list[i-1],depth_list[i]))
+            if use_norm:
+                self.linear_list.append(nn.BatchNorm1d(depth_list[i]))
             self.linear_list.append(nn.ReLU(depth_list[i]))
             # self.linear_list.append(nn.Tanh())
+            # self.linear_list.append(nn.Dropout(0.2))
+            
 
         self.linear = nn.Sequential(*self.linear_list)
         self.drop = nn.Dropout(drop_prob) if drop_prob > 0.0 else None
@@ -135,9 +141,9 @@ def train_epoch(epoch,net,criterion,optim,train_loader,scaler,use_fp16=True):
 
         torch.cuda.empty_cache()
 
-        if step % 10 == 0:
-            print('epoch:{},step:{},train_loss:{:.5f},train_acc:{:.5f},lr:{}'
-                    .format(epoch, step, loss.item(), acc.item(), optim.param_groups[0]['lr']))
+        # if step % 10 == 0:
+        #     print('epoch:{},step:{},train_loss:{:.5f},train_acc:{:.5f},lr:{}'
+        #             .format(epoch, step, loss.item(), acc.item(), optim.param_groups[0]['lr']))
 
     return train_acc.avg,train_loss.avg
 
@@ -176,8 +182,6 @@ def val_epoch(epoch,net,criterion,val_loader,use_fp16=True):
     return val_acc.avg,val_loss.avg
 
 
-
-
 def evaluation(test_data,net,weight_path,use_fp16=True):
     ckpt = torch.load(weight_path)
     net.load_state_dict(ckpt['state_dict'])
@@ -195,9 +199,6 @@ def evaluation(test_data,net,weight_path,use_fp16=True):
     return output.cpu().numpy().tolist()
 
 
-
-
-
 def eval_metric(result_dict,test_id,pred_result,le):
     true_result = [result_dict[case] for case in test_id]
     true_result = le.transform(true_result)
@@ -211,13 +212,37 @@ def eval_metric(result_dict,test_id,pred_result,le):
     return acc,f1
 
 
-def main():
+def manual_select(total_list,exclude_list=None):
+    fea_list = []
+    nouse_list = []
+    if exclude_list is not None:
+        for exclude_label in exclude_list:
+            if 'sum' not in exclude_label and 'avg' not in exclude_label:
+                nouse_list += [f'{exclude_label}_{str(i)}' for i in range(400)]
+            else:
+                nouse_list += [f'{exclude_label}_{str(i)}' for i in range(40)]
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '7'
-    output_dir = './model/'
+        for col in total_list:
+            if col not in nouse_list:
+                fea_list.append(col)
+    
+        return fea_list
+    else:
+        return total_list
+
+
+
+def run(net_depth=3,exclude_list=None):
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '6'
+    torch.manual_seed(0)
+
+    output_dir = f'./model_exclude/'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-
+    else:
+        shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
     # load training and testing data
     train_path = '../dataset/pssm_train.csv'
     train_df = pd.read_csv(train_path)
@@ -235,10 +260,13 @@ def main():
     del test_df['seq']
 
     test_id = test_df['id']
+    # print(list(test_id))
     del train_df['id']
     del test_df['id']
 
-    fea_list = [f for f in train_df.columns if f not in ['label']] 
+    # manual select
+    fea_list = manual_select(train_df.columns,exclude_list)
+    fea_list = [f for f in fea_list if f not in ['label']]
 
     le = LabelEncoder()
     train_df['label'] = le.fit_transform(train_df['label'])
@@ -250,6 +278,16 @@ def main():
     X = np.asarray(train_df[fea_list]).astype(np.float32)
     test = np.asarray(test_df[fea_list]).astype(np.float32)
 
+    # feature selection
+    # select_model_path = './select_model.pkl'
+    # if os.path.exists(select_model_path):
+    #     with open(select_model_path, 'rb') as f:
+    #         select_model = pickle.load(f)
+    # else:
+    #     select_model = select_feature_linesvc(X, Y, select_model_path)
+
+    # X = select_model.transform(X)
+    # test = select_model.transform(test)
     # print(Y)
     print(X.shape,test.shape)
 
@@ -266,7 +304,7 @@ def main():
         epoch_num = 100
         acc_threshold = 0.0
 
-        depth = 4
+        depth = net_depth
         depth_list = [int(fea_len*(2**(1-i))) for i in range(depth)]
         
         net = MLP_CLASSIFIER(fea_len,num_classes,depth,depth_list)
@@ -340,8 +378,8 @@ def main():
         # csv save
         fold_csv = {}
         fold_csv = pd.DataFrame(fold_csv)
-        fold_csv['sample_id'] = test_id + ['d2ciob_']
-        fold_csv['category_id'] = fold_result + ['b.1']
+        fold_csv['sample_id'] = list(test_id) + ['d2ciob_']
+        fold_csv['category_id'] = list(fold_result) + ['b.1']
         fold_csv.to_csv(os.path.join(output_dir, f'fold{fold_num}_acc-{round(acc,4)}_f1-{round(f1,4)}.csv'),index=False)
 
     # result fusion by voting
@@ -354,9 +392,15 @@ def main():
     # csv save
     total_csv = {}
     total_csv = pd.DataFrame(total_csv)
-    total_csv['sample_id'] = test_id + ['d2ciob_']
-    total_csv['category_id'] = final_result + ['b.1']
+    total_csv['sample_id'] = list(test_id) + ['d2ciob_']
+    total_csv['category_id'] = list(final_result) + ['b.1']
     total_csv.to_csv(os.path.join(output_dir, f'fusion_acc-{round(acc,4)}_f1-{round(f1,4)}.csv'),index=False)
 
+
 if __name__ == '__main__':
-    main()
+    pssm_key = ['pssm_sum', 'pssm_avg','acc_1','acc_2','acc_3','acc_4','acc_5','acc_6','sxg_0','sxg_1','sxg_2','sxg_3','sxg_4','sxg_5']
+    
+    for i in range(10):
+        exclude_list = random.sample(pssm_key[2:],6)
+        print('exclude list:',exclude_list)
+        run(net_depth=3,exclude_list=exclude_list)
